448dfd1d4717ca69318133a12b174be76f56f474
mfks17 <mfks17@gmail.com>
2014-01-15 19:54:40 +0900

    Update liya.rb

--- Casks/liya.rb
+++ Casks/liya.rb
@@ -1,7 +1,7 @@
 class Gyazo < Cask
   url 'http://cutedgesystems.com/downloads/Liya.zip'
   homepage 'http://cutedgesystems.com/'
-  version '2.0i.13'
+  version '2.0.13'
   no_checksum
   link 'Liya.app'
 end
